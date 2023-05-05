699e3dc47783215dc0536e053243901cb32b0c87
Raresstefan <raresstefan.rosu@gmail.com>
2023-05-05 20:35:52 +0300

    Test method

--- skel/solver_neopt.c
+++ skel/solver_neopt.c
@@ -9,15 +9,23 @@
  */
 double* my_solver(int N, double *A, double* B) {
 	double *C = calloc(N * N, sizeof(double));
+	// for (int i = 0; i < N; i++) {
+	// 	for (int j = i; j < N; j++) {
+	// 		double sum = 0;
+	// 		for (int k = i; k <= j; k++) {
+	// 			if (A[i * N + k] != 0 && B[k * N + j] != 0) {
+	// 				sum += A[i * N + k] * B[k * N + j];
+	// 			}
+	// 		}
+	// 		C[i * N + j] = sum;
+	// 	}
+	// }
 	for (int i = 0; i < N; i++) {
-		for (int j = i; j < N; j++) {
-			double sum = 0;
-			for (int k = i; k <= j; k++) {
-				if (A[i * N + k] != 0 && B[k * N + j] != 0) {
-					sum += A[i * N + k] * B[k * N + j];
-				}
+		for (int j = 0; j < N; j++) {
+			C[i * N + j] = 0;
+			for (int k = 0; k < N; k++) {
+				C[i * N + j] += A[i * N + k] * B[k * N + j];
 			}
-			C[i * N + j] = sum;
 		}
 	}
 	return C;
