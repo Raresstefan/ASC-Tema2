cf79a6d8e0d705fc0fc82d9f7704a5d813d4ced6
Raresstefan <raresstefan.rosu@gmail.com>
2023-05-05 18:55:00 +0300

    Added solver_neopt.c

--- skel/solver_neopt.c
+++ skel/solver_neopt.c
@@ -9,5 +9,15 @@
  */
 double* my_solver(int N, double *A, double* B) {
 	printf("NEOPT SOLVER\n");
-	return NULL;
+	double *C = calloc(N * N, sizeof(double));
+	for (int i = 0; i < N; i++) {
+		for (int j = i; j < N; j++) {
+			double sum = 0;
+			for (int k = i; k <= j; k++) {
+				sum += A[i * N + k] * B[k * N + j];
+			}
+			C[i * N + j] = sum;
+		}
+	}
+	return C;
 }
