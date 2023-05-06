24dae3c50be1e9eae6a0a2acfd05a01a79d47128
Raresstefan <raresstefan.rosu@gmail.com>
2023-05-06 19:17:41 +0300

    ppaps

--- skel/solver_blas.c
+++ skel/solver_blas.c
@@ -24,7 +24,7 @@ double* my_solver(int N, double *A, double *B) {
 	cblas_dgemm(CblasRowMajor, CblasTrans, CblasTrans, N, N, N, 1.0, B, N, B, N, 1.0, B_T_B_T, N);
 
 	for (int i = 0; i < N;i ++) {
-		for (int j = i; j < N; j++) {
+		for (int j = 0; j < N; j++) {
 			C[i * N + j] = ABA_T[i * N + j] + B_T_B_T[i * N + j];
 		}
 	}
